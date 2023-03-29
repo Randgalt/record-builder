183ab67c1ae25526f00c9a01cdaa2f163d207f6e
Stefan Bischof <bipolis@bipolis.org>
2023-03-29 09:53:06 +0200

    handle RecordBuilder.Options on packages (#149)
    Thanks for the PR

--- record-builder-core/src/main/java/io/soabase/recordbuilder/core/RecordBuilder.java
+++ record-builder-core/src/main/java/io/soabase/recordbuilder/core/RecordBuilder.java
@@ -15,8 +15,13 @@
  */
 package io.soabase.recordbuilder.core;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
 import javax.lang.model.element.Modifier;
-import java.lang.annotation.*;
 
 @Retention(RetentionPolicy.SOURCE)
 @Target(ElementType.TYPE)
@@ -58,7 +63,7 @@ public @interface RecordBuilder {
     }
 
     @Retention(RetentionPolicy.SOURCE)
-    @Target(ElementType.TYPE)
+    @Target({ElementType.TYPE, ElementType.PACKAGE})
     @Inherited
     @interface Options {
         /**
